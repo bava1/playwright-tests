8256b154c64e14f9c74b84534153e3041090ab2a
bavastudio <vinus133@yahoo.com>
2024-11-05 20:42:53 +0100

    add blog

---
+++ tests/blog/sign-Up.spec.ts
@@ -0,0 +1,23 @@
+const { test, expect } = require('@playwright/test');
+import { Page } from '@playwright/test';
+
+test('blog auth sign in', async ({ page }: { page: Page }) => {
+  // your code here
+  await page.waitForTimeout(1000);
+  await page.goto('http://blog.ba-va.eu/');
+  await page.getByRole('button', { name: 'Sign In' }).click();
+  await page.getByPlaceholder('email').click();
+  await page.getByPlaceholder('email').fill('test@test.com');
+  await page.getByPlaceholder('password').click();
+  await page.getByPlaceholder('password').fill('654321');
+  await page.locator('form').getByRole('button', { name: 'Sign Up' }).click();
+  await page.waitForTimeout(5000);
+  await page.locator('svg').click();
+  await page.waitForTimeout(1000);
+  await page.getByRole('button', { name: 'Log out' }).click();
+  await page.waitForTimeout(3000);
+
+});
+
+// test@test.com
+// 123456
--- tests/test-ts.test.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('Example test Typescript', async ({ page }) => {
-  await page.goto('https://bavastudio.eu/#/');
-  await page.waitForTimeout(5000);
-  const title = await page.title();
-  expect(title).toBe('bavastudio design art 3D paint');
-});
